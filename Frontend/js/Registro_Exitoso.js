96695656a2310efff6892495c103902b5fd26e42
johanquispe455-art <johanquispe455@gmail.com>
2026-01-13 15:54:16 -0500

    Delete Frontend/js directory

--- Frontend/js/Registro_Exitoso.js
+++
@@ -1,4 +0,0 @@
-// Script extraído de Registro_Exitoso.html
-setTimeout(() => {
-  window.location.href = "Registrar_Denuncia.html";
-}, 4000);
--- Frontend/js/panel_admin.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const logoutBtn = document.getElementById('logoutBtn');
-
-    logoutBtn.addEventListener('click', () => {
-        if (confirm('¿Estás seguro de que deseas cerrar sesión?')) {
-            window.location.href = 'index.html'; // Redirigir al login o inicio
-        }
-    });
-});
