597f6cc7412bbc0520c6340a078b2af1aa58eb2e
johanquispe455-art <johanquispe455@gmail.com>
2026-01-13 16:05:12 -0500

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import path from "path"; // Importante para las rutas de archivos
 
 import authRoutes from "./Rutas/auth.routes.js";
-import dniRoutes from "./Servicios/dni.route.js"; 
+import dniRoutes from "./Rutas/dni.route.js"; 
 import denunciaRoutes from "./Rutas/denuncia.routes.js";
 import incidenciaRoutes from "./Rutas/incidencias.routes.js"; // 1. IMPORTAR RUTAS DE INCIDENCIAS (EL MAPA)
 import adminUsuarioRoutes from "./Rutas/adminusuario.routes.js";
@@ -48,4 +48,5 @@ app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 app.use("/api/admin-incidencias", adminIncidenciasRoutes);
+
 
