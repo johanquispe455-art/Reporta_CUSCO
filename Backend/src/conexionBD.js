ae0bbb3042953a82296436bccaa3295dd69fdc94
johanquispe455-art <johanquispe455@gmail.com>
2026-01-13 15:33:26 -0500

    Refactor conexionBD.js for improved logging

--- Backend/src/conexionBD.js
+++ Backend/src/conexionBD.js
@@ -1,28 +1,20 @@
-console.log("🟡 EJECUTANDO conexionBD.js");
-
 import pkg from "pg";
 const { Pool } = pkg;
 
+console.log("🟡 EJECUTANDO conexionBD.js");
+console.log("🔥 CONFIG PG -> family = 4");
+
 export const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_URL ||
-    "postgresql://postgres:kiri74493570to@db.wrvcqmyyjzemgagalzub.supabase.co:5432/postgres",
-
-  ssl: {
-    rejectUnauthorized: false,
-  },
-
-  family: 4, // 🔥 FORZAR IPv4 (CLAVE PARA RENDER)
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  family: 4, // 👈 ESTO ES LO IMPORTANTE
 });
 
-// Test de conexión
 pool.connect((err, client, release) => {
   if (err) {
     console.error("❌ Error de conexión a Supabase:", err);
     return;
   }
-
   console.log("✅ Conexión exitosa a la base de datos en la nube");
   release();
 });
-
