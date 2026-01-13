7cec51a535fd1882a267c3394f6c1b7081bfca1a
johanquispe455-art <johanquispe455@gmail.com>
2026-01-13 14:56:13 -0500

    Refactor database connection to use connection string
    Updated database connection to use connection string from environment variable or default value. Added connection check with logging.

--- Backend/src/conexionBD.js
+++ Backend/src/conexionBD.js
@@ -1,11 +1,19 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+// Render usa una "connectionString" (un solo texto con todo)
+// Si no existe, usa tus datos de Supabase que ya tienes
 export const pool = new Pool({
-  host: "db.wrvcqmyyjzemgagalzub.supabase.co",
-  user: "postgres",
-  password: "kiri74493570to",
-  database: "postgres",
-  port: 5432,
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL || "postgresql://postgres:kiri74493570to@db.wrvcqmyyjzemgagalzub.supabase.co:5432/postgres",
+  ssl: { 
+    rejectUnauthorized: false 
+  }
+});
+// Añade esto al final de tu archivo conexionBD.js
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('❌ Error de conexión a Supabase:', err.message);
+  }
+  console.log('✅ Conexión exitosa a la base de datos en la nube');
+  release();
 });
