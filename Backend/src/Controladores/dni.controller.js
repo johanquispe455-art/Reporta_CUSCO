7fdffbe77edcdaeee1a37e89a3a31d64b854aa25
johanquispe455-art <johanquispe455@gmail.com>
2026-01-13 16:09:20 -0500

    Update dni.controller.js

--- Backend/src/Controladores/dni.controller.js
+++ Backend/src/Controladores/dni.controller.js
@@ -1,1 +1,20 @@
+import { obtenerNombrePorDni } from "../Servicios/dniScraper.js";
 
+export const consultarDni = async (req, res) => {
+  const { dni } = req.params;
+
+  if (!dni || dni.length !== 8) {
+    return res.status(400).json({ ok: false, mensaje: "DNI inválido" });
+  }
+
+  const nombre = await obtenerNombrePorDni(dni);
+
+  if (!nombre) {
+    return res.json({ ok: false, mensaje: "DNI no encontrado" });
+  }
+
+  res.json({
+    ok: true,
+    nombre_completo: nombre.toUpperCase()
+  });
+};
